8991603d23b540f4c193d1f0f245d760765b39fb
Lentor3579 <louie.torres.e@gmail.com>
2018-09-04 19:50:30 -0400

    Test project
    Just a test project to see if repository is working

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(speed * gameObject.GetComponent<InputProcessor>().dir, 0);
+	}
+}
