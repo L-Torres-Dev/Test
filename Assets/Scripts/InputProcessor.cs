eb7d6b975f6be008b4ec9dd247224630f9b1f15b
L-Torres-Dev <louie.torres.e@gmail.com>
2019-01-06 09:24:39 -0500

    Revert "First Commit"
    This reverts commit 37cf606d11b8df59b8de9b45b1df5309c48228ea.

---
+++ Assets/Scripts/InputProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputProcessor : MonoBehaviour {
+
+    [HideInInspector]
+    public float dir;
+
+    private bool isFacingRight;
+
+	// Use this for initialization
+	void Start () {
+        isFacingRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            dir = 1;
+            gameObject.GetComponent<Animator>().SetBool("isMoving", true);
+            isFacingRight = true;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir = -1;
+            gameObject.GetComponent<Animator>().SetBool("isMoving", true);
+            isFacingRight = false;
+        }
+
+        else
+        {
+            dir = 0;
+            gameObject.GetComponent<Animator>().SetBool("isMoving", false);
+        }
+
+        Flip();
+    }
+
+    protected void Flip()
+    {
+        if (!isFacingRight)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+}
